3bfcb37331219fd7ace00a5db0e20904abe5f790
Sebastian Dunn <sebastiancdunn@byui.edu>
2022-11-01 12:45:48 -0600

    added Program.cs

--- Unit03/Director.cs
+++ Unit03/Director.cs
@@ -5,21 +5,20 @@ namespace Unit03
 {
     class Director
     {
-        static void Main(string[] args)
+        public void startGame()
         {
             Jumper createMan = new Jumper();
-            TerminalService userInputs = new TerminalService();
-            createMan.createJumper();
-            char playAgain = 'y';
-            /// While loop that deals with inputs and changes the output accordingly
-            do
-            {
-                userInputs.UserInput();
-                Console.WriteLine("Play again? (y/n)");
-                playAgain = Console.ReadKey().KeyChar;
-            } while (playAgain == 'y');
-
-            Console.WriteLine("Thanks for playing!");
+                TerminalService userInputs = new TerminalService();
+                createMan.createJumper();
+                char playAgain = 'y';
+                /// While loop that deals with inputs and changes the output accordingly
+                do
+                {
+                    userInputs.UserInput();
+                    Console.WriteLine("Play again? (y/n)");
+                    playAgain = Console.ReadKey().KeyChar;
+                } while (playAgain == 'y');
+                Console.WriteLine("Thanks for playing!");
         }
     }
 }
